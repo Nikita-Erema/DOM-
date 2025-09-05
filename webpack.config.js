2a62f79614e3aadfce378b8a6682193dc6edead7
Archick <k0bana@mail.ru>
2025-09-05 16:14:55 +0300

    actions commit

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     entry: './src/app.js',
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'docs')
     },
     mode: 'development',
     plugins: [new HtmlWebpackPlugin({
